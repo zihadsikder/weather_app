abe281743bbb406c4c19ed9a2b2ab1543553753d
zihadsikder <zihadsikder.cse@gmail.com>
2024-12-05 14:19:35 +0600

    Implement functionality to cache the fetched data locally using a local database(Hive).

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -4,49 +4,7 @@ import 'package:hive/hive.dart';
 import '../../../data/models/weather_model.dart';
 import '../../../data/models/network_response.dart';
 import '../../../domain/repositories/weather_repository.dart';
-//
-// class HomeController extends GetxController {
-//   final isLoading = false.obs;
-//   final weather = Rx<WeatherModel?>(null);
-//
-//   final weatherBox = Hive.box<WeatherModel>('weatherBox');
-//
-//   @override
-//   void onInit() {
-//     super.onInit();
-//     fetchWeather("Dhaka");
-//     loadWeather();
-//   }
-//
-//   Future<void> loadWeather() async {
-//     if (weatherBox.isNotEmpty) {
-//       // Load cached data if available
-//       weather.value = weatherBox.values as WeatherModel;
-//     } else {
-//       await fetchWeather("Dhaka");
-//     }
-//   }
-//
-//   Future<void> fetchWeather(String cityName) async {
-//     isLoading.value = true;
-//
-//     final NetworkResponse response =
-//         await WeatherRepository.fetchWeather(cityName);
-//     if (response.isSuccess) {
-//       final fetchedWeather = weatherModelFromJson(response.jsonResponse!);
-//
-//       // Save to Hive for offline access
-//       await weatherBox.clear(); // Clear old data
-//       //await weatherBox.addAll(fetchedWeather as Iterable<WeatherModel>);
-//
-//       // Update the observable
-//       weather.value = fetchedWeather;
-//     } else {
-//       Get.snackbar("Error", response.message ?? "Failed to fetch weather data");
-//     }
-//     isLoading.value = false;
-//   }
-// }
+
 class HomeController extends GetxController {
   final isLoading = false.obs;
   final weather = Rx<WeatherModel?>(null);
@@ -56,7 +14,7 @@ class HomeController extends GetxController {
   @override
   void onInit() async {
     super.onInit();
-    await initHive();  // Initialize and load the box
+    await initHive(); // Initialize and load the box
     fetchWeather("Dhaka");
     loadWeather();
   }
@@ -79,13 +37,13 @@ class HomeController extends GetxController {
     isLoading.value = true;
 
     final NetworkResponse response =
-    await WeatherRepository.fetchWeather(cityName);
+        await WeatherRepository.fetchWeather(cityName);
     if (response.isSuccess) {
       final fetchedWeather = weatherModelFromJson(response.jsonResponse!);
 
       // Save to Hive for offline access
       await weatherBox.clear(); // Clear old data
-      await weatherBox.add(fetchedWeather);  // Save the fetched weather
+      await weatherBox.add(fetchedWeather); // Save the fetched weather
 
       // Update the observable
       weather.value = fetchedWeather;
